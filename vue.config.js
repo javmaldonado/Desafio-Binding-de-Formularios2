6e8e806e7417faf332db62bcd5e2437a4340ad6a
spl-javmaldonado <javiera.maldonado.agurto@gmail.com>
2024-06-27 22:28:04 -0400

    Second

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:"/binding-de-formularios2/"
+base:"/binding-de-formularios2/",
+plugins:[vue()]
+ 
 })
